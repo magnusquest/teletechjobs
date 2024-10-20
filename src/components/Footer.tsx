da4cd32eff53b7631ce1a9b99ee19be4f9f1f3c0
magnusquest <thisismartinc@gmail.com>
2024-10-20 14:18:58 -0400

    footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ import Phone from "@/components/Phone";
 
 const Footer = () => {
   return (
-    <footer className="w-full h-36 py-4 px-12">
-      <div className="h-1/3" />
-      <div className="grid grid-cols-2 items-center justify-center px-24">
+    <footer className="w-full h-full py-4 px-12">
+      <div className="hidden md:block h-1/3" />
+      <div className="lg:grid lg:grid-cols-2 items-center justify-center px-24">
         <div className="flex items-center justify-center">
           <Email />
           <h4>info@teletechjobs.us</h4>
